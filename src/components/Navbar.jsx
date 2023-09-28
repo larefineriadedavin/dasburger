c3e2c6cf022d357ebe45e2f9cdef8d689d45bfb7
larefineriadedavin <refineriap9@gmail.com>
2023-09-28 17:30:17 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,13 @@
 export default function Navbar() {
     return (
         <nav>
-            <h1>DASBURGER</h1>
-
+            <h1 className="nav--logo">DASBURGER</h1>
+            <ul className="nav--menu">
+                <li>Menu</li>
+                <li>Acerca De</li>
+                <li>Contacto</li>
+            </ul>
+            <a>Ordenar</a>
         </nav>
     )
 }
