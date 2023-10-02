cd23e4607fa5a20fd3a65fc934275ccb5650a6c8
larefineriadedavin <refineriap9@gmail.com>
2023-10-02 11:43:56 -0600

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 function App() {
   
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <section className="hero">
+            <h3 className="hero--ad">LO NUEVO</h3>
+            <h1 className="hero--title">HAMBURGUESAS DE TEMPORADA</h1>
+            <a className="hero--btn">ORDER NOW</a>         
+        </section>
+    )
+}
