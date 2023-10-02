a226660963171344b5d4d2d1a8bdd05b0d4626c7
larefineriadedavin <refineriap9@gmail.com>
2023-10-02 17:49:20 -0600

    images

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
 import './App.css'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Content from "./components/Content"
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Content />
     </div>
   )
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,31 @@
+// import myImage from '../assets/images/frame.png'
+
+
+export default function Content() {
+    return (
+        <section className="content">
+            <ul className="content--menu">
+                <li>HAMBURGUESAS</li>
+                <li>PAPAS</li>
+                <li>PERROS CALIENTES</li>
+            </ul>
+            <div className="menu">
+                <div className='menu--card'>
+                    <img></img>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div className='menu--card'>
+                    <img></img>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <div className='menu--card'>
+                    <img></img>
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+        </section>
+    )
+}
