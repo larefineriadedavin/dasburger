d6c73389bb5dc6adf8a545b4a049363ec4849091
larefineriadedavin <refineriap9@gmail.com>
2023-10-03 17:03:27 -0600

    ad promo

--- src/components/ContentAd.jsx
+++ src/components/ContentAd.jsx
@@ -2,7 +2,7 @@ export default function ContentAd() {
     return (
         <div className="ad">
             <div className="ad--one">
-                <h3 className="ad--promo"></h3>
+                <h3 className="ad--promo">DESCARGA LA APP</h3>
                 <h1></h1>
                 <p></p>
             </div>
