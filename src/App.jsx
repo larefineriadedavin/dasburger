db53ef497522086a912e91bb3b3e2e48b3e46421
larefineriadedavin <refineriap9@gmail.com>
2023-10-03 16:45:03 -0600

    changes ad

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Content from "./components/Content"
+import ContentAdd from './components/ContentAdd'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <Content />
+      <ContentAdd />
     </div>
   )
 }
---
+++ src/components/ContentAdd.jsx
@@ -0,0 +1,14 @@
+export default function ContentAdd() {
+    return (
+        <div className="add">
+            <div className="add--one">
+                <h3></h3>
+                <h1></h1>
+                <p></p>
+            </div>
+            <div className="add--two">
+                <img></img>
+            </div>
+        </div>
+    )
+}
