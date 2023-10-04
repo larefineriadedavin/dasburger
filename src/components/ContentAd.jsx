fcce2ea57a1e60a0945e12e578bbdd1b17d70c5f
larefineriadedavin <refineriap9@gmail.com>
2023-10-04 10:37:18 -0600

    ad title

--- src/components/ContentAd.jsx
+++ src/components/ContentAd.jsx
@@ -3,7 +3,7 @@ export default function ContentAd() {
         <div className="ad">
             <div className="ad--one">
                 <h3 className="ad--promo">DESCARGA LA APP</h3>
-                <h1></h1>
+                <h1 className="ad--title">Ùnete a Free Pass</h1>
                 <p></p>
             </div>
             <div className="ad--two">
