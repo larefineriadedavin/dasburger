a8c419f4cee7d913eec1605c2a87f86819de0239
larefineriadedavin <refineriap9@gmail.com>
2023-10-04 14:54:01 -0600

    ad

--- src/components/ContentAd.jsx
+++ src/components/ContentAd.jsx
@@ -3,8 +3,14 @@ export default function ContentAd() {
         <div className="ad">
             <div className="ad--one">
                 <h3 className="ad--promo">DESCARGA LA APP</h3>
-                <h1 className="ad--title">Ùnete a Free Pass</h1>
-                <p></p>
+                <h1 className="ad--title">Únete a Free Pass</h1>
+                <p className="ad--text">La app FreePass para IOS y para Android es la forma más conveniente para obtener alimentos frescos, deliciosos y agradables a pedido. 
+                Descargue la aplicación y ordene para recoger 
+                y entregar.
+                Desbloquee un nuevo y delicioso mundo de beneficios cuando se una a nuestro programa de
+                lealtad gratuito, 
+                FreePass, y gane recompensas, regalos de cumpleaños, menus exclusivos y mas.
+                </p>
             </div>
             <div className="ad--two">
                 <img></img>
