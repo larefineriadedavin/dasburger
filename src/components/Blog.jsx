017413380ec33f413867dd97c733814554a96aae
larefineriadedavin <refineriap9@gmail.com>
2023-10-06 17:08:42 -0600

    blog read

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,8 +5,8 @@ export default function Blog() {
         <div className="blog">
             <div className="blog--title">
                 <h3>LO ÚLTIMO</h3>
-                <img src='arrow-right-solid.svg' className="r--arrow"></img>
-                <img src='arrow-left-solid.svg' className="l--arrow"></img>
+                <img src='right.svg' className="r--arrow"></img>
+                <img src='left.svg' className="l--arrow"></img>
             </div>
             <div className="card--blog">
                 <h3>GOOGLE MAPS</h3>
@@ -21,7 +21,7 @@ export default function Blog() {
                 <p>Una vez vine con mi mejor amiga, de verdad que el sitio era de su agrado, siempre me lo recomendó, ahora veo porque le gustaba tanto.</p>
                 <img className="card--image2"></img>
             </div>
-            
+            <h3 className="blog--read">LEER MAS</h3>
         </div>
     )
 }
