6dfd75fbe3b6e21b38eb77b4bbe684073e9c3fef
larefineriadedavin <refineriap9@gmail.com>
2023-10-06 15:36:48 -0600

    images

--- src/components/ContentMenu.jsx
+++ src/components/ContentMenu.jsx
@@ -15,12 +15,12 @@ export default function ContentMenu() {
                     <h3 className="menu--title">HAMBURGUESA CLASICA</h3>
                     <p className="menu--text">Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor.</p>
                 </div>
-                <div className='menu--card'>
+                <div className='menu--card2'>
                     <img></img>
                     <h3 className="menu--title">PAPAS A LA FRANCESA</h3>
                     <p className="menu--text">Salchicha Frankfurt, dos lonchas de queso, dos hojas de lechuga, un cuarto de tomate, un cuarto de cebolla morada y un cuarto de aguacate, media lima, sal, aceite de oliva y queso parmesano rallado.</p>
                 </div>
-                <div className='menu--card'>
+                <div className='menu--card3'>
                     <img></img>
                     <h3 className="menu--title">PERRO COMPLETO</h3>
                     <p className="menu--text">Nuestras ricas papas a la francesa con lemon pepper son las preferidas por nuestra clientela que les fascina el sabor y la mezcla de limon..</p>
