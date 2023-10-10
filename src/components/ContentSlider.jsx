12227e27701a27b942a4901f736cd33894a9a910
larefineriadedavin <refineriap9@gmail.com>
2023-10-10 16:16:54 -0600

    changes

--- src/components/ContentSlider.jsx
+++ src/components/ContentSlider.jsx
@@ -2,9 +2,9 @@ export default function ContentSlider() {
     return (
        <div className="content--slider">
             <div className="content--slider1">
-                <h3>***BURGER EXCLUSIVOS</h3>
+                <h3>DASBURGER EXCLUSIVOS</h3>
                 <h1>Sumérgete en los favoritos de los fans</h1>
-                <p>Ordene a menudo y siempre sea un éxito, estas selecciones populares son verdaderos productos básicos de ***burger.
+                <p>Ordene a menudo y siempre sea un éxito, estas selecciones populares son verdaderos productos básicos de Dasburger.
                 esta colección incluye la deliciosa limonada, brownies y todo tipo de postres para todos los gustos.</p>
             </div>
             <div className="content--slider2">
