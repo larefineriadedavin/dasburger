a1a75a885f2a4e1ae9cccc00e474a09806f4843b
larefineriadedavin <refineriap9@gmail.com>
2023-11-03 10:20:45 -0600

    images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function Card(props) {
     return (
         <section className="card">
-            <img src={props.imageSrc} alt={props.imageAlt} className="card-image" />
+            <img src={`/public/images/${props.imageSrc}`} alt={props.imageAlt} className="card-image" />
             <h3 className="card-title">{props.title}</h3>
             <p className="card-text">{props.text}</p>
         </section>
--- src/datamenu.js
+++ src/datamenu.js
@@ -1,42 +1,72 @@
 export default [
     {
         id: 1,
-        cardImage: "/public/images/pexels-valeria-boltneva-1639562.jpg",
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
         title: "HAMBURGUESA CLASICA",
         text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
     },
 
     {
         id: 2,
-        cardImage: "/public/images/pexels-valeria-boltneva-1639562.jpg",
-        title: "HAMBURGUESA CLASICA",
-        text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
+        cardImage: "sahand-babali-R9eP-0I96qo-unsplash.jpg",
+        imageAlt: "picture of a burger",
+        title: "PAPAS A LA FRANCESA",
+        text: "Nuestras ricas papas a la francesa con lemon pepper son las preferidas por nuestra clientela que les fascina el sabor y la mezcla de limon.",   
     },
 
     {
         id: 3,
-        cardImage: "/public/images/pexels-valeria-boltneva-1639562.jpg",
-        title: "HAMBURGUESA CLASICA",
-        text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
+        cardImage: "david-todd-mccarty-Z2cAwmIKDxA-unsplash.jpg",
+        imageAlt: "picture of a burger",
+        title: "PERRO COMPLETO",
+        text: "Salchicha Frankfurt, dos lonchas de queso, dos hojas de lechuga, un cuarto de tomate, un cuarto de cebolla morada y un cuarto de aguacate, media lima, sal, aceite de oliva y queso parmesano rallado."
     },
 
     {
         id: 4,
-        cardImage: "/public/images/pexels-valeria-boltneva-1639562.jpg",
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
         title: "HAMBURGUESA CLASICA",
         text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
     },
 
     {
         id: 5,
-        cardImage: "/public/images/pexels-valeria-boltneva-1639562.jpg",
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
         title: "HAMBURGUESA CLASICA",
         text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
     },
 
     {
         id: 6,
-        cardImage: "/public/images/pexels-valeria-boltneva-1639562.jpg",
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
+        title: "HAMBURGUESA CLASICA",
+        text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
+    },
+
+    {
+        id: 7,
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
+        title: "HAMBURGUESA CLASICA",
+        text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
+    },
+
+    {
+        id: 8,
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
+        title: "HAMBURGUESA CLASICA",
+        text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
+    },
+
+    {
+        id: 9,
+        cardImage: "pexels-valeria-boltneva-1639562.jpg",
+        imageAlt: "picture of a burger",
         title: "HAMBURGUESA CLASICA",
         text: "Carne picada de ternera, pan humedecido en leche, huevo, tocineta de cerdo y otros condimentos constituyen la base de un excelente sabor."
     }
