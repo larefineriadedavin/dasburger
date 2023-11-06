458e8120b6539f9f7aa9ed6c79aa1f3fb9ef8ff2
larefineriadedavin <refineriap9@gmail.com>
2023-11-06 13:06:08 -0600

    cursors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@
 export default function Hero() {
     return (
         <section className="hero">
-            <img src="/public/images/jonathan-borba-8l8Yl2ruUsg-unsplash.jpg" alt="image of burger and fries" className="hero--image" />
+            <img src="/images/jonathan-borba-8l8Yl2ruUsg-unsplash.jpg" alt="image of burger and fries" className="hero--image" />
             <h3 className="hero--ad">LO NUEVO</h3>
             <h1 className="hero--title">HAMBURGUESAS DE TEMPORADA</h1>
             <a className="hero--btn">ORDER NOW</a>         
